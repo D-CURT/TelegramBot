6fde58c995aa1cd384828729e271b422757b4326
AlexeyShevko <alexey@pollentechnologies.com>
2023-10-10 19:00:32 +0600

    Scheduler unfinished added;

--- src/main/java/com/task/telegram/scheduler/ScheduledTask.java
+++ src/main/java/com/task/telegram/scheduler/ScheduledTask.java
@@ -1,5 +1,6 @@
 package com.task.telegram.scheduler;
 
+import com.task.telegram.bot.TelegramBot;
 import com.task.telegram.dao.TelegramUserRepository;
 import com.task.telegram.dto.crypto.CryptoValue;
 import com.task.telegram.model.telegram.TelegramUser;
@@ -8,7 +9,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
@@ -18,6 +23,7 @@ public class ScheduledTask {
 
     public static final int INTERVAL = 20000;
 
+    private final TelegramBot bot;
     private final CryptoService cryptoService;
     private final TelegramUserRepository telegramUserRepository;
 
@@ -25,5 +31,11 @@ public class ScheduledTask {
     public void updatePrices() {
         List<CryptoValue> prices = cryptoService.getPrices();
         Iterable<TelegramUser> users = telegramUserRepository.findAll();
+        for (TelegramUser user: users) {
+            LocalDateTime created = user.getCreatedAt();
+            //TODO fix;
+            Update update = new Update();
+            bot.onUpdateReceived(update);
+        }
     }
 }
